f27fff4d06f7514884c671010cd27cf1baf50483
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-08-14 16:52:33 -0500

    Initial commit. Add instsructions to the README and add subproblems to the rb file

---
+++ stockpicker.rb
@@ -0,0 +1,4 @@
+#Subproblems to solve: 
+  #Look at the stock and choose the lowest number, this is the best day to buy 
+  #From the buy date forward, choose the highest number, this is the best day to sell 
+  #Subtract the sell price from the buy price to calculate the profit 
